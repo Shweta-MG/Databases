5e17146243244822743916b701a7f209654438f1
Shweta <shweta.malav@gmail.com>
2023-10-27 15:04:26 +0200

    CRUD exercises were performed and then new file for string exercieses created.

--- MySQL_Commands/MySQL-CRUD Command_Part2.sql
+++ MySQL_Commands/MySQL-CRUD Command_Part2.sql
@@ -117,3 +117,77 @@ Delete from cats where cat_age=5;
 
 Use Animals;
 select * from cats;
+
+
+--CRUD exercise day 2
+create database Clothes;
+
+show databases;
+
+use Clothes;
+create table Shirts (
+    Shirts_id INT AUTO_INCREMENT PRIMARY KEY,
+    Shirts_type VARCHAR(100) NOT NULL,
+    Shirts_color VARCHAR(100) NOT NULL,
+    Shirts_size VARCHAR(10) NOT NULL,
+    Last_worn VARCHAR(100)
+);
+
+use Clothes;
+INSERT INTO Shirts (Shirts_type, Shirts_color, Shirts_size, Last_worn)
+VALUES
+    ('T-Shirt', 'Red', 'M', '2023-10-20'),
+    ('Polo Shirt', 'Blue', 'L', '2023-10-18'),
+    ('Dress Shirt', 'White', 'S', '2023-10-19'),
+    ('Tank Top', 'Black', 'XL', NULL),
+    ('Flannel Shirt', 'Plaid', 'M', '2023-10-17'),
+    ('Hoodie', 'Gray', 'L', '2023-10-15'),
+    ('Sweatshirt', 'Green', 'M', '2023-10-20'),
+    ('Button-Up Shirt', 'Striped', 'XL', '2023-10-16'),
+    ('Pullover Sweater', 'Navy', 'L', NULL),
+    ('Turtleneck', 'Brown', 'M', '2023-10-14'),
+    ('Long-Sleeve Shirt', 'Yellow', 'S', '2023-10-13'),
+    ('Henley', 'Maroon', 'L', '2023-10-12'),
+    ('V-Neck Sweater', 'Teal', 'XL', '2023-10-11'),
+    ('Graphic Tee', 'Black', 'M', '2023-10-10'),
+    ('Sleeveless Blouse', 'Pink', 'S', '2023-10-09'),
+    ('Hawaiian Shirt', 'Hawaiian Print', 'M', NULL),
+    ('Oxford Shirt', 'Blue', 'L', '2023-10-08'),
+    ('Tunic', 'Purple', 'XL', '2023-10-07'),
+    ('Tie-Dye Shirt', 'Rainbow', 'M', '2023-10-06'),
+    ('Fitted Shirt', 'Gray', 'S', '2023-10-05');
+
+use Clothes;
+select * from Shirts;
+
+
+
+use Clothes;
+DESC Shirts;
+
+--add one shirt
+use Clothes;
+INSERT INTO Shirts (Shirts_type, Shirts_color, Shirts_size, Last_worn)
+VALUES
+    ('T-Shirt', 'Green', 'XS', '2023-10-30'),
+
+--print only shirt-type and shirt color 
+use Clothes;
+select Shirts_type as article, Shirts_color as color from Shirts where Shirts_size='S'
+
+--update polo shirt size to L
+use Clothes;
+update Shirts SET Shirts_size='L' where Shirts_type='T-Shirt';
+
+--update last-worn
+use Clothes;
+update Shirts SET Last_worn='0' where Last_worn='2023-10-05';
+
+--Delete from shirt where size is S
+use Clothes;
+Delete from Shirts where Shirts_size='s';
+
+use Clothes;
+DRop table Shirts;
+
+drop database Clothes;
